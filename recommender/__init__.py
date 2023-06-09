49b4a48645426ccf3056b7db46850d43404eeda1
arbezerra <arbezerra@gmail.com>
2023-06-09 17:26:48 -0300

    Shap

--- recommender/__init__.py
+++ recommender/__init__.py
@@ -2,6 +2,7 @@ from .consts import TAMANHO_MINIMO_SVM
 from .quadtree import QuadTree
 from .svm import SVM
 from .entropy import EntropyCalculator
+import pandas as pd
 from pandas import DataFrame
 
 class Recommender:
@@ -9,8 +10,8 @@ class Recommender:
         self.sample_size = sample_size
         self.entropy_calculator = EntropyCalculator()
 
-    def init_app(self, data):
-        self.data = data
+    def init_app(self, data: DataFrame):
+        self.data = pd.get_dummies(data, columns=["type", "fabric", "pattern", "size", "color"], dtype=float)
 
     def getData(self):
         return self.data.to_json()
@@ -19,20 +20,20 @@ class Recommender:
         df = self.data.copy()
         for key in slots:
             if len(slots[key]) > 0:
-                df = df[df[key].isin(slots[key])]
+                df = df.loc[df[key].isin(slots[key])]
         return df
 
     def recommend(self, state):
         filtered = self.filter_slots(state['slots'])
-        liked = filtered[filtered['name'].isin(state['liked'])]
+        liked = filtered.loc[filtered['name'].isin(state['liked'])]
         liked["class"] = 1
         
-        if len(liked) < TAMANHO_MINIMO_SVM:
+        if liked.shape[0] < TAMANHO_MINIMO_SVM:
             return self.recommend_random(filtered)
         return self.recommend_svm(filtered, liked)
 
     def recommend_svm(self, filtered: DataFrame, liked: DataFrame):
-        filtered = filtered[~filtered['name'].isin(liked['name'])]
+        filtered = filtered.loc[~filtered['name'].isin(liked['name'])]
         svm = SVM(filtered, liked)
         return svm.select(self.sample_size)
     
--- recommender/svm.py
+++ recommender/svm.py
@@ -1,17 +1,44 @@
 from .quadtree import QuadTree
 from sklearn.svm import OneClassSVM
+from threading import Thread
 import pandas as pd
+import shap
+import requests
+import os
+
+FEATURES = ['type_pleated', 'type_skewed',
+            'type_straight', 'fabric_denim', 'fabric_general', 'fabric_knitted',
+            'fabric_lace', 'fabric_leather', 'fabric_velvet',
+            'pattern_animal_print', 'pattern_camouflage', 'pattern_checked',
+            'pattern_floral', 'pattern_geometric', 'pattern_paisley',
+            'pattern_plain', 'pattern_polka_dot', 'pattern_striped',
+            'pattern_tie_dyed', 'size_maxi', 'size_midi', 'size_mini',
+            'color_beige', 'color_black', 'color_blue', 'color_brown', 'color_gray',
+            'color_green', 'color_orange', 'color_pink', 'color_purple',
+            'color_red', 'color_white', 'color_yellow']
+
+
+def calculate_shap(explainer, x):
+    shap_values = explainer.shap_values(x, nsampels=100)
+    requests.post(
+        os.getenv('CHATBOT_API')+"/log", json={"id": "recommender", "data": {"shap": shap_values.tolist(), "expected": explainer.expected_value, "columns": FEATURES}})
 
 
 class SVM:
     def __init__(self, df, liked):
-        x = liked[["x", "y"]]
+        x = liked[FEATURES]
         y = liked["class"]
+        x_test = df[FEATURES]
 
         svc = OneClassSVM(gamma="auto", nu=0.01)
-        svc.fit(x, y)
+        svc.fit(x.values, y)
 
-        predicted = svc.decision_function(df[["x", "y"]])
+        predicted = svc.decision_function(x_test.values)
+
+        explainer = shap.KernelExplainer(svc.decision_function, x.values)
+
+        t = Thread(target=calculate_shap, args=(explainer, x_test.values))
+        t.start()
 
         df["class"] = predicted
         df = df.sort_values(by=['class'], ascending=False)
