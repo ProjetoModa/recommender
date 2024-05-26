e61a26bef63be8b732d9a8eb73422f2f90f75917
arbezerra <arbezerra@gmail.com>
2024-05-26 19:57:59 -0300

    fix: sample size

--- recommender/quadtree.py
+++ recommender/quadtree.py
@@ -2,14 +2,20 @@ from .consts import EPS
 from .point import Point
 from .region import Region
 import random
-            
+
+
 class QuadTree:
     def __init__(self, data):
         self.data = data
         self.max_points = 6
         self.max_depth = 10
-        self.root = Region(data['x'].min(), data['y'].min(),
-                                          data['x'].max() + EPS, data['y'].max() + EPS, self._convert_points(data))
+        self.root = Region(
+            data['x'].min(),
+            data['y'].min(),
+            data['x'].max() + EPS,
+            data['y'].max() + EPS,
+            self._convert_points(data),
+        )
         self.create_tree(self.root)
 
     def _convert_points(self, data):
@@ -23,42 +29,42 @@ class QuadTree:
     def create_tree(self, region: Region, depth=0):
         if len(region) <= self.max_points or depth >= self.max_depth:
             return
-        
+
         for i in range(4):
             region.children.append(region.select_quad(i))
-        
+
         for child in region.children:
-            self.create_tree(child, depth+1)
+            self.create_tree(child, depth + 1)
 
     def select(self, sample_size):
-        
+
         sample_size = min(sample_size, len(self.root))
-        
+
         points = []
         for child in self.root.children:
-            self.sample(child, min(sample_size//4, len(child)), points)
-        
-        self.sample(self.root, sample_size - len(points), points)
-        
+            points += self.sample(child, min(sample_size // 4, len(child)))
+
+        self.sample(self.root, sample_size, points)
+
         indexes = [point.idx for point in points]
         selected = self.data.loc[indexes, :]
         return selected['name'].values.tolist()
-    
-    def sample(self, region: Region, n, samples):
-            
+
+    def sample(self, region: Region, n, samples=[]):
+
         while len(samples) < n:
             point = self.search(region)
             if point not in samples:
                 samples.append(point)
-                
+
         return samples
-    
+
     def search(self, region: Region):
         if len(region.children) == 0:
             return random.choice(region.points)
-        
+
         for child in random.sample(region.children, len(region.children)):
             if len(child.points) > 0:
                 return self.search(child)
-            
+
         return None
